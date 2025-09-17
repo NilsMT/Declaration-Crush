f04da13f3e23c1de76549bc8ddea7cc7cd58f8e4
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2025-09-18 00:09:53 +0200

    themes n stuff

--- site/src/assets/js/constants/debugConstant.js
+++
@@ -1,4 +0,0 @@
-export const DebugConstant = Object.freeze({
-    DEBUG_ENABLED: true,
-    DEBUG_DISABLED: false,
-});
--- site/src/assets/js/constants/themeConstant.js
+++ site/src/assets/js/constants/themeConstant.js
@@ -1,10 +1,26 @@
 export const ThemeConstant = Object.freeze({
+    RED: {
+        name: "red",
+        icon: "favorite",
+    },
     PINK: {
         name: "pink",
         icon: "heart_plus",
     },
-    RED: {
-        name: "red",
-        icon: "favorite",
+    PURPLE: {
+        name: "purple",
+        icon: "heart_minus",
+    },
+    CYAN: {
+        name: "cyan",
+        icon: "relax",
+    },
+    GREEN: {
+        name: "green",
+        icon: "ecg_heart",
+    },
+    YELLOW: {
+        name: "yellow",
+        icon: "heart_smile",
     },
 });
